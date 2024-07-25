e4c15e722af97001e6dbfacad6a565142a478504
may-jr <138032771+may-jr@users.noreply.github.com>
2024-07-25 13:24:07 +0000

    trying to link the pages

--- Public/Js/script.js
+++ Public/Js/script.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const registerBtn = document.querySelector('.register-btn');
     registerBtn.addEventListener('click', () => {
-        window.location.href = '../public/pages/registration.html';
+        window.location.href = '../Public/pages/registation.html';
     });
 
     const contactForm = document.querySelector('form');
